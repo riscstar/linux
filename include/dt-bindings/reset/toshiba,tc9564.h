bd48a1563126782db6db52390ee2a96fb54174f9
Alex Elder <elder@riscstar.com>
2026-04-27 17:38:12 +0100

    dt-bindings: reset: introduce toshiba,tc9564-reset.yaml
    Define the binding for the reset controller functionality present in
    the Toshiba TC9564 SoC.
    
    Signed-off-by: Daniel Thompson <daniel@riscstar.com>
    Signed-off-by: Alex Elder <elder@riscstar.com>

---
+++ include/dt-bindings/reset/toshiba,tc9564.h
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+
+/*
+ * Copyright (C) 2026 by RISCstar Solutions Corporation.  All rights reserved.
+ */
+
+#ifndef __TOSHIBA_TC9564_RESETS_H__
+#define __TOSHIBA_TC9564_RESETS_H__
+
+#define RESET_MCU	0
+#define RESET_MCU1	1
+#define RESET_MSIGEN	2
+#define RESET_INTC	3
+#define RESET_UART	4
+/* #define RESET_I2C	5 */
+/* #define RESET_PCIE	6 */
+
+#define RESET_MAC0_MAC	7
+#define RESET_MAC0_PMA	8
+#define RESET_MAC0_XPCS	9
+
+#define RESET_MAC1_MAC	10
+#define RESET_MAC1_PMA	11
+#define RESET_MAC1_XPCS	12
+
+#endif /* __TOSHIBA_TC9564_RESETS_H__*/
